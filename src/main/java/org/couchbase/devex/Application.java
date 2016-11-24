f5e8586cf1a0ad0cd9c55d5d853c1599e5bec3ee
Laurent Doguin <laurent.doguin@couchbase.com>
2016-11-24 16:08:31 +0100

    add failing example

--- src/main/java/org/couchbase/devex/Application.java
+++ src/main/java/org/couchbase/devex/Application.java
@@ -1,11 +1,16 @@
 package org.couchbase.devex;
 
 import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.query.N1qlQuery;
+import com.couchbase.client.java.util.rawQuerying.RawQueryExecutor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class Application {
@@ -24,4 +29,24 @@ public class Application {
 			System.out.println(bucket.name());
 		};
 	}
+
+
+	@Bean
+	public RawQueryExecutor rawQueryExecutor(final Bucket bucket){
+		return  new RawQueryExecutor(bucket.name(), "", bucket.core(), bucket.environment());
+	}
+}
+
+@Controller
+class IndexController {
+
+	@Autowired
+	private Bucket bucket;
+
+	@RequestMapping("/query/")
+	@ResponseBody
+	public String doQuery(RawQueryExecutor rawQueryExecutor) {
+		return rawQueryExecutor.n1qlToRawJson(N1qlQuery.simple("SELECT * FROM " + bucket.name()));
+	}
+
 }
